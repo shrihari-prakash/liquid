650393c1d0f4005d4a338dc29b185e93e324d26f
Shrihari Prakash <35889246+Shrihari-Prakash@users.noreply.github.com>
2022-12-20 23:48:09 +0530

    #3: Implemented client level APIs for following and followers listing. (#4)

--- src/service/api/user/client-api/routes.ts
+++ src/service/api/user/client-api/routes.ts
@@ -1,10 +1,14 @@
 import express from "express";
 
 import { ClientAuthFlow } from "../../middleware/authenticate";
-import GetUserInfo from "./get-user-info";
+import UserInfo from "./get-user-info";
+import UserFollowers from "./user-followers";
+import UserFollowing from "./user-following";
 
 const ClientApiRouter = express.Router();
 
-ClientApiRouter.post("/get-user-info", ...ClientAuthFlow, GetUserInfo);
+ClientApiRouter.post("/get-user-info", ...ClientAuthFlow, UserInfo);
+ClientApiRouter.get("/user-following", ...ClientAuthFlow, UserFollowing);
+ClientApiRouter.get("/user-followers", ...ClientAuthFlow, UserFollowers);
 
 export default ClientApiRouter;
