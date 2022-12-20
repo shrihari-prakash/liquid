3147417986cbcc5b31dc394bd2b1cbfb6ff98e35
Shrihari Prakash <shrihariprakash@outlook.com>
2022-12-20 21:32:26 +0530

    Implemented logic to auto accept pending follow requests when going public.

--- src/service/api/user/private.ts
+++ src/service/api/user/private.ts
@@ -8,6 +8,7 @@ import { errorMessages, statusCodes } from "../../../utils/http-status";
 import { ErrorResponse, SuccessResponse } from "../../../utils/response";
 import UserModel from "../../../model/mongo/user";
 import { validateErrors } from "../../../utils/api";
+import FollowModel from "../../../model/mongo/follow";
 
 export const SwitchPrivateValidator = [body("state").exists().isBoolean()];
 
@@ -17,6 +18,30 @@ const SwitchPrivate = async (req: Request, res: Response) => {
     const userId = res.locals.oauth.token.user._id;
     const state = req.body.state;
     await UserModel.updateOne({ _id: userId }, { $set: { isPrivate: state } });
+    // Accept all pending follow requests.
+    if (state === false) {
+      const requestsToApprove = await FollowModel.find({
+        $and: [{ targetId: userId }, { approved: false }],
+      });
+      if (requestsToApprove.length) {
+        const result = (await FollowModel.updateMany(
+          { targetId: userId },
+          { $set: { approved: true } }
+        )) as any;
+        // Update follower count for the user going public.
+        await UserModel.updateOne(
+          { _id: userId },
+          { $inc: { followerCount: result.modifiedCount } }
+        );
+        // Update folowing counts of the users whose requests were approved.
+        await UserModel.updateMany(
+          {
+            _id: { $in: requestsToApprove.map((request) => request.sourceId) },
+          },
+          { $inc: { followingCount: 1 } }
+        );
+      }
+    }
     res.status(statusCodes.success).json(new SuccessResponse());
   } catch (err) {
     log.error(err);
