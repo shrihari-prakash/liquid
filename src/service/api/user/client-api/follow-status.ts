a7585c5cb1a74a5f106ed797de4b24de8088b428
Shrihari Prakash <35889246+Shrihari-Prakash@users.noreply.github.com>
2022-12-22 21:36:42 +0530

    #2: Implemented client level APIs for retrieving follow status. (#6)
    * Added client level API for retrieving follow status.
    
    * Added swagger definitions.

---
+++ src/service/api/user/client-api/follow-status.ts
@@ -0,0 +1,30 @@
+import { Logger } from "../../../../singleton/logger";
+const log = Logger.getLogger().child({ from: "client-api/user-following" });
+
+import { Request, Response } from "express";
+
+import { errorMessages, statusCodes } from "../../../../utils/http-status";
+import { ErrorResponse, SuccessResponse } from "../../../../utils/response";
+import FollowModel from "../../../../model/mongo/follow";
+import { useFollowingQuery } from "../../../../model/query/following";
+import { IUser } from "../../../../model/mongo/user";
+
+const FollowStatus = async (req: Request, res: Response) => {
+  try {
+    const sourceId = req.query.source as string;
+    const targetId = req.query.target as string;
+    const isFollowing = (await FollowModel.findOne({
+      $and: [{ targetId }, { sourceId }],
+    }).exec()) as unknown as IUser;
+    res
+      .status(statusCodes.success)
+      .json(new SuccessResponse({ following: isFollowing ? true : false }));
+  } catch (err) {
+    log.error(err);
+    return res
+      .status(statusCodes.internalError)
+      .json(new ErrorResponse(errorMessages.internalError));
+  }
+};
+
+export default FollowStatus;
--- src/service/api/user/client-api/routes.ts
+++ src/service/api/user/client-api/routes.ts
@@ -1,6 +1,7 @@
 import express from "express";
 
 import { ClientAuthFlow } from "../../middleware/authenticate";
+import FollowStatus from "./follow-status";
 import UserInfo from "./get-user-info";
 import UserFollowers from "./user-followers";
 import UserFollowing from "./user-following";
@@ -8,6 +9,7 @@ import UserFollowing from "./user-following";
 const ClientApiRouter = express.Router();
 
 ClientApiRouter.post("/get-user-info", ...ClientAuthFlow, UserInfo);
+ClientApiRouter.get("/follow-status", ...ClientAuthFlow, FollowStatus);
 ClientApiRouter.get("/user-following", ...ClientAuthFlow, UserFollowing);
 ClientApiRouter.get("/user-followers", ...ClientAuthFlow, UserFollowers);
 
