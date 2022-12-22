cbe49d28c541d1a7314e2fc8a059f09039ab1f81
Shrihari Prakash <shrihariprakash@outlook.com>
2022-12-22 21:50:26 +0530

    Adjust fields returned in get user info API.

--- src/service/api/user/_userId.ts
+++ src/service/api/user/_userId.ts
@@ -12,17 +12,39 @@ const _UserId = async (req: Request, res: Response) => {
   try {
     const targetId = req.params.userId;
     const sourceId = res.locals.oauth.token.user._id;
-    const user = (await UserModel.findOne({
-      _id: targetId,
-    }).exec()) as unknown as IUser;
+    const user = (await UserModel.findOne(
+      { _id: targetId },
+      {
+        _id: 1,
+        username: 1,
+        firstName: 1,
+        middleName: 1,
+        lastName: 1,
+        profilePictureUrl: 1,
+        followingCount: 1,
+        followerCount: 1,
+        isPrivate: 1,
+        email: 1,
+        phone: 1,
+        secondaryEmail: 1,
+        secondaryPhone: 1,
+        isBanned: 1,
+        isRestricted: 1,
+      }
+    ).exec()) as unknown as IUser;
     if (user.isPrivate) {
       const isFollowing = (await FollowModel.findOne({
         $and: [{ targetId }, { sourceId }],
       }).exec()) as unknown as IUser;
       if (!isFollowing) {
-        return res
-          .status(statusCodes.forbidden)
-          .json(new ErrorResponse(errorMessages.accessDenied));
+        // @ts-expect-error
+        delete user.email;
+        // @ts-expect-error
+        delete user.phone;
+        // @ts-expect-error
+        delete user.secondaryEmail;
+        // @ts-expect-error
+        delete user.secondaryPhone;
       }
     }
     res.status(statusCodes.success).json(new SuccessResponse({ user }));
