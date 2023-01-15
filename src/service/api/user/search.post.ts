d10949e38be656e42355bae178c712c7d19c5fed
Shrihari Prakash <shrihariprakash@outlook.com>
2023-01-15 22:32:26 +0530

    Optimized search API with Redis usage.

--- src/service/api/user/search.post.ts
+++ src/service/api/user/search.post.ts
@@ -1,5 +1,5 @@
 import { Logger } from "../../../singleton/logger";
-const log = Logger.getLogger().child({ from: "user/followers" });
+const log = Logger.getLogger().child({ from: "user/search" });
 
 import { Request, Response } from "express";
 
@@ -8,15 +8,27 @@ import { ErrorResponse, SuccessResponse } from "../../../utils/response";
 import UserModel, { IUserProjection } from "../../../model/mongo/user";
 import { body } from "express-validator";
 import { validateErrors } from "../../../utils/api";
+import { Redis } from "../../../singleton/redis";
+import { Configuration } from "../../../singleton/configuration";
 
 export const POST_SearchValidator = [
   body("query").exists().isString().isLength({ max: 128 }),
 ];
 
+const redisPrefix = "SEARCH_";
 const POST_Search = async (req: Request, res: Response) => {
   validateErrors(req, res);
   try {
     const query = req.body.query;
+    if (Configuration.get("privilege.can-use-cache")) {
+      const cacheResults = await Redis.client.get(`${redisPrefix}${query}`);
+      if (cacheResults) {
+        return res
+          .status(statusCodes.success)
+          .json(new SuccessResponse({ results: JSON.parse(cacheResults) }));
+      }
+    }
+    log.info("Cache miss for query: " + query);
     const queryRegex = new RegExp(query, "i");
     const results = await UserModel.find(
       {
@@ -28,6 +40,14 @@ const POST_Search = async (req: Request, res: Response) => {
       },
       IUserProjection
     );
+    if (Configuration.get("privilege.can-use-cache")) {
+      await Redis.client.set(
+        `${redisPrefix}${query}`,
+        JSON.stringify(results),
+        "EX",
+        Configuration.get("user.search-results.cache-lifetime") as number
+      );
+    }
     res.status(statusCodes.success).json(new SuccessResponse({ results }));
   } catch (err) {
     log.error(err);
