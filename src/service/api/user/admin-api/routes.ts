a0e667b323a29167626e74cbf8955ad1cbdb1f47
Shrihari Prakash <shrihariprakash@outlook.com>
2023-03-18 00:06:24 +0530

    Converted ban API to shared resource.

--- src/service/api/user/admin-api/routes.ts
+++ src/service/api/user/admin-api/routes.ts
@@ -2,6 +2,7 @@ import express from "express";
 
 import { DelegatedAuthFlow } from "../../middleware/authenticate";
 import AuthorizeAdmin from "../../middleware/authorize-admin";
+import POST_Banned, { POST_BannedValidator } from "../shared/banned.post";
 import POST_Access, { POST_AccessValidator } from "./access.post";
 import GET_EditableFields from "./editable-fields.get";
 import PATCH_User, { PATCH_UserValidator } from "./user.patch";
@@ -11,5 +12,6 @@ const AdminApiRouter = express.Router();
 AdminApiRouter.post("/access", ...DelegatedAuthFlow, AuthorizeAdmin, POST_AccessValidator, POST_Access);
 AdminApiRouter.get("/editable-fields", ...DelegatedAuthFlow, AuthorizeAdmin, GET_EditableFields);
 AdminApiRouter.patch("/user", ...DelegatedAuthFlow, AuthorizeAdmin, PATCH_UserValidator, PATCH_User);
+AdminApiRouter.post("/banned", ...DelegatedAuthFlow, AuthorizeAdmin, POST_BannedValidator, POST_Banned);
 
 export default AdminApiRouter;
--- src/service/api/user/client-api/routes.ts
+++ src/service/api/user/client-api/routes.ts
@@ -2,7 +2,7 @@ import express from "express";
 
 import { Configuration } from "../../../../singleton/configuration";
 import { ClientAuthFlow } from "../../middleware/authenticate";
-import POST_Banned, { POST_BannedValidator } from "./banned.post";
+import POST_Banned, { POST_BannedValidator } from "../shared/banned.post";
 import GET_FollowStatus, { GET_FollowStatusValidator } from "./follow-status.get";
 import GET_UserInfo from "./user-info.get";
 import GET_UserFollowers, { GET_UserFollowersValidator } from "./user-followers.get";
