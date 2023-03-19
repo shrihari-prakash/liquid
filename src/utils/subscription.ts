d8be095fc39396e991117e1e5b629b9fd66b7334
Shrihari Prakash <shrihariprakash@outlook.com>
2023-03-19 14:08:17 +0530

    Added update query when finding expired subscriptions.

--- src/utils/subscription.ts
+++ src/utils/subscription.ts
@@ -1,19 +1,24 @@
 import moment from "moment";
-import { IUser } from "../model/mongo/user";
+import UserModel, { IUser } from "../model/mongo/user";
 
 export const checkSubscription = (input: IUser | IUser[]) => {
   if (Array.isArray(input)) {
+    const toUpdate = [];
     const users = input;
     for (let i = 0; i < users.length; i++) {
       if (users[i].isSubscribed && moment().isAfter(moment(users[i].subscriptionExpiry))) {
         users[i].isSubscribed = false;
+        toUpdate.push(users[i]._id);
       }
     }
+    console.log(toUpdate);
+    toUpdate.length && UserModel.updateMany({ _id: { $in: toUpdate } }, { $set: { isSubscribed: false } }, () => {});
     return users;
   } else {
     const user = input;
     if (user.isSubscribed && moment().isAfter(moment(user.subscriptionExpiry))) {
       user.isSubscribed = false;
+      UserModel.updateOne({ _id: user._id }, { $set: { isSubscribed: false } }, () => {});
     }
     return user;
   }
