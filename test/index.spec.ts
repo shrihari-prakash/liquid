e81f2a2da27ccf78bc4a364b13f61d1376f15b76
Shrihari Prakash <shrihariprakash@outlook.com>
2023-04-08 00:02:48 +0530

    Fixed MongoDB local not working in node 19.

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,5 +1,5 @@
 process.env.NODE_ENV = "test";
-process.env.MONGO_DB_CONNECTION_STRING = "mongodb://localhost:27017/liquid-test";
+process.env.MONGO_DB_CONNECTION_STRING = "mongodb://127.0.0.1:27017/liquid-test";
 process.env.REDIS_DB = "1";
 process.env.SYSTEM_LOG_LEVEL = "error";
 
