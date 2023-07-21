ee07b7b0b92ba8e659130b848cb7173d83db6ec8
Shrihari Prakasam <Shrihari.Prakasam@genesys.com>
2023-07-21 09:53:22 +0530

    Added wait time to invite code response.

--- src/service/api/user/invite-codes.get.ts
+++ src/service/api/user/invite-codes.get.ts
@@ -16,12 +16,13 @@ const GET_InviteCodes = async (_: Request, res: Response) => {
     const difference = durationSinceCreation.asSeconds();
     const window = Configuration.get("user.account-creation.invite-code-availability-window");
     if (difference <= window) {
+      const waitTime = window - difference;
       log.info(
         "Hidden invite codes for account %s. %s seconds left for releasing invite codes.",
         user._id,
-        window - difference
+        waitTime
       );
-      return res.status(statusCodes.clientInputError).json(new ErrorResponse(errorMessages.inviteCodesHidden));
+      return res.status(statusCodes.clientInputError).json(new ErrorResponse(errorMessages.inviteCodesHidden, { waitTime: Math.ceil(waitTime) }));
     }
     let inviteCodes = await InviteCodeModel.find({ sourceId: user._id }, { sourceId: 0, __v: 0, _id: 0 }).lean().exec();
     res.status(statusCodes.success).json(new SuccessResponse({ inviteCodes }));
