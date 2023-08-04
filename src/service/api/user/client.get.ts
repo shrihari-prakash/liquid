12e885241672171c523119c95dcc7ce934240d56
Shrihari Prakash <shrihariprakash@outlook.com>
2023-08-04 22:39:07 +0530

    Added missing field validation in get client API.

--- src/service/api/user/client.get.ts
+++ src/service/api/user/client.get.ts
@@ -7,11 +7,13 @@ import { query } from "express-validator";
 import { errorMessages, statusCodes } from "../../../utils/http-status";
 import { ErrorResponse, SuccessResponse } from "../../../utils/response";
 import ClientModel from "../../../model/mongo/client";
+import { hasErrors } from "../../../utils/api";
 
 export const GET_ClientValidator = [query("id").exists().isString().isLength({ min: 3, max: 128 })];
 
 const GET_Client = async (req: Request, res: Response) => {
   try {
+    if (hasErrors(req, res)) return;
     const id = req.query.id;
     const client = (await ClientModel.findOne({ id }, { id: 1, role: 1, displayName: 1, _id: 0 }).lean().exec()) as any;
     res.status(statusCodes.success).json(new SuccessResponse({ client }));
