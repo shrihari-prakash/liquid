e6a4f783d53e077a91c4aa4dc47fc7f13a771c3e
Shrihari Prakash <shrihariprakash@outlook.com>
2023-08-19 19:39:55 +0530

    Fixed test setup process.

--- test/integration/index.spec.ts
+++ test/integration/index.spec.ts
@@ -7,17 +7,21 @@ const mongod = new MongoMemoryServer({
     port: 1111,
   },
 });
-import { MongoDB } from "../src/singleton/mongo-db";
-import ClientModel from "../src/model/mongo/client";
-import { Logger } from "../src/singleton/logger";
-import Options from "../src/service/configuration/options.json";
-import { Configuration } from "../src/singleton/configuration";
+import { MongoDB } from "../../src/singleton/mongo-db";
+import ClientModel from "../../src/model/mongo/client";
+import { Logger } from "../../src/singleton/logger";
+import Options from "../../src/service/configuration/options.json";
+import { Configuration } from "../../src/singleton/configuration";
+
+process.env.NODE_ENV = "test";
+process.env.CAN_USE_CACHE = "false";
 
 Options.forEach((option) => {
+  // Couldn't make Redis work with the tests unfortunately :(
+  if (option.name === "environment" || option.name === "privilege.can-use-cache") return;
   if (typeof option.default !== "undefined") Configuration.set(option.name, option.default);
   console.log(`${option.name} =`, Configuration.get(option.name));
 });
-process.env.NODE_ENV = "test";
 Logger.logger.level = "error";
 chai.use(chaiHttp);
 
