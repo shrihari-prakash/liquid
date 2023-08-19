6c58b1d516a6939672e94d546dab2be87188b917
Shrihari Prakash <shrihariprakash@outlook.com>
2023-08-19 19:41:11 +0530

    Adjusted test index formatting.

--- test/integration/index.spec.ts
+++ test/integration/index.spec.ts
@@ -13,8 +13,11 @@ import { Logger } from "../../src/singleton/logger";
 import Options from "../../src/service/configuration/options.json";
 import { Configuration } from "../../src/singleton/configuration";
 
+chai.use(chaiHttp);
+
 process.env.NODE_ENV = "test";
 process.env.CAN_USE_CACHE = "false";
+Logger.logger.level = "error";
 
 Options.forEach((option) => {
   // Couldn't make Redis work with the tests unfortunately :(
@@ -22,8 +25,6 @@ Options.forEach((option) => {
   if (typeof option.default !== "undefined") Configuration.set(option.name, option.default);
   console.log(`${option.name} =`, Configuration.get(option.name));
 });
-Logger.logger.level = "error";
-chai.use(chaiHttp);
 
 before(async () => {
   await mongod.start();
