38ca2de36b563ce1b896abd33bd1dee984fdb717
Shrihari Prakash <shrihariprakash@outlook.com>
2023-08-25 15:12:16 +0530

    Redis publisher cleanup.

--- src/service/redis-publisher/redis-publisher.ts
+++ src/service/redis-publisher/redis-publisher.ts
@@ -1,9 +1,5 @@
-import { Logger } from "../../singleton/logger";
-const log = Logger.getLogger().child({ from: "rabbitmq" });
-
 import { Configuration } from "../../singleton/configuration";
 import Redis from "../redis/redis";
-import RedisFake from "../redis/redis-fake";
 
 export default class RedisPublisher {
   redis: Redis | undefined;
@@ -16,14 +12,8 @@ export default class RedisPublisher {
 
   async connect() {
     this.canUseCache = Configuration.get("privilege.can-use-cache");
-    try {
-      if (this.canUseCache && Configuration.get("environment") !== "test") {
-        this.redis = new Redis("redis-publisher");
-      } else {
-        this.redis = new RedisFake() as unknown as Redis;
-      }
-    } catch (error) {
-      log.error(error);
+    if (this.canUseCache && Configuration.get("environment") !== "test") {
+      this.redis = new Redis("redis-publisher");
     }
   }
 
