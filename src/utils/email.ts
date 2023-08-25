43718720bc5f8403578232afaed9baba534dcb52
Shrihari Prakash <shrihariprakash@outlook.com>
2023-08-25 21:00:15 +0530

    Improved gmail address sanitization.

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -6,6 +6,7 @@ export const sanitizeEmailAddress = (email: string) => {
   // https://support.google.com/mail/answer/7436150
   if (email.endsWith("@gmail.com") && Configuration.get("user.account-creation.sanitize-gmail-addresses")) {
     const parts = email.split("@");
+    parts[0] = parts[0].split("+")[0];
     return parts[0].replace(/\./g, "") + "@" + parts[1];
   }
   return email;
