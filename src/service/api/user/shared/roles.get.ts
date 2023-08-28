90f4db2f192846bbdd0f07cb83d227fb0ef10a96
Shrihari Prakash <shrihariprakash@outlook.com>
2023-08-28 20:47:37 +0530

    Added additional details about role in roles API.

--- src/service/api/user/shared/roles.get.ts
+++ src/service/api/user/shared/roles.get.ts
@@ -7,13 +7,23 @@ import { errorMessages, statusCodes } from "../../../../utils/http-status";
 import { ErrorResponse, SuccessResponse } from "../../../../utils/response";
 import Role from "../../../../enum/role";
 import { ScopeManager } from "../../../../singleton/scope-manager";
+import { extractRank, findRoleRank } from "../../../../utils/role";
 
 const GET_Roles = async (_: Request, res: Response) => {
   try {
     if (!ScopeManager.isScopeAllowedForSharedSession("user.<ENTITY>.configuration.read", res)) {
       return;
     }
-    const roles = Object.values(Role);
+    const roles = Object.values(Role).map((role) => {
+      return {
+        name: role,
+        displayName: (role as string)
+          .split("_")
+          .map((s: string) => s.charAt(0).toUpperCase() + s.slice(1))
+          .join(" "),
+        rank: parseInt(extractRank(findRoleRank(role as string))),
+      };
+    });
     res.status(statusCodes.success).json(new SuccessResponse({ roles }));
   } catch (err) {
     log.error(err);
--- src/utils/role.ts
+++ src/utils/role.ts
@@ -1,16 +1,16 @@
 import { Configuration } from "../singleton/configuration";
 
-export const exractRank = (roleRank: string) => {
+export const extractRank = (roleRank: string) => {
   return (roleRank.match(/\(([^)]+)\)/) as string[])[1];
 };
 
-const findRoleRank = (role: string) => {
+export const findRoleRank = (role: string) => {
   const roleRanking = Configuration.get("system.role.ranking");
   return roleRanking.find((r: string) => r.split("(")[0] === role);
 };
 
 export const isRoleRankHigher = (currentRole: string, comparisonRole: string) => {
-  const currentRoleRank = exractRank(findRoleRank(currentRole));
-  const comparisonRoleRank = exractRank(findRoleRank(comparisonRole));
+  const currentRoleRank = extractRank(findRoleRank(currentRole));
+  const comparisonRoleRank = extractRank(findRoleRank(comparisonRole));
   return parseInt(currentRoleRank) < parseInt(comparisonRoleRank);
 };
