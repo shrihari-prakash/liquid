a1f89261805698fd2a2b7f6fd4ac2c4cee7fc713
Shrihari Prakash <shrihariprakash@outlook.com>
2023-09-06 21:42:37 +0530

    Added option to control access to introspection API for external clients.

--- src/service/api/oauth/introspect.ts
+++ src/service/api/oauth/introspect.ts
@@ -3,12 +3,18 @@ import { Request, Response } from "express";
 import { errorMessages, statusCodes } from "../../../utils/http-status";
 import { ErrorResponse, SuccessResponse } from "../../../utils/response";
 import OAuthModel from "../../../model/oauth";
+import Role from "../../../enum/role";
 import { ScopeManager } from "../../../singleton/scope-manager";
 import { UserProjection } from "../../../model/mongo/user";
+import { Configuration } from "../../../singleton/configuration";
 
 const ALL_Introspect = async (req: Request, res: Response) => {
   if (!ScopeManager.isScopeAllowedForSession("client:oauth:read", res)) {
     return;
+  }
+  const role = res.locals.user.role;
+  if (role === Role.EXTERNAL_CLIENT && !Configuration.get("oauth.external-client.allow-introspection")) {
+    return res.status(statusCodes.forbidden).json(new ErrorResponse(errorMessages.forbidden));
   }
   const errors: any[] = [];
   if (
