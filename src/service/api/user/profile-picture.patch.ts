0219c8ebe2ad0c6002dd5e61b029d7463316cc2c
Shrihari Prakash <shrihariprakash@outlook.com>
2023-09-24 00:16:47 +0530

    Adjusted profile picture API.

--- src/service/api/user/profile-picture.patch.ts
+++ src/service/api/user/profile-picture.patch.ts
@@ -50,7 +50,7 @@ if (Configuration.get("privilege.can-use-cloud-storage")) {
   };
 }
 
-const uploadProfilePicture = profilePictureMulter.single("profile-picture");
+const uploadProfilePicture = profilePictureMulter.single("profilePicture");
 
 export const profilePicturePath = `${Configuration.get("storage.cloud-path")}/profile-pictures`;
 
