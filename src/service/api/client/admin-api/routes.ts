b0f1d2dfb440a757ef2b53be5a9a32c40dff82aa
Shrihari Prakasam <Shrihari.Prakasam@genesys.com>
2023-11-21 18:08:46 +0530

    Refactor API filenames.

--- src/service/api/client/admin-api/routes.ts
+++ src/service/api/client/admin-api/routes.ts
@@ -1,11 +1,11 @@
 import express from "express";
 import { DelegatedAuthFlow } from "../../middleware/authenticate";
-import POST_Client, { POST_ClientValidator } from "./client.post";
+import POST_Create, { POST_CreateValidator } from "./create";
 import GET_List from "./list.get";
 
 const ClientAdminRouter = express.Router();
 
-ClientAdminRouter.post("/create", ...DelegatedAuthFlow, POST_ClientValidator, POST_Client);
+ClientAdminRouter.post("/create", ...DelegatedAuthFlow, POST_CreateValidator, POST_Create);
 ClientAdminRouter.get("/list", ...DelegatedAuthFlow, GET_List);
 
 export default ClientAdminRouter;
