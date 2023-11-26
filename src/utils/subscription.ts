60ebbec6235c7e51ee9067dcc57d1213463db16e
Shrihari Prakash <shrihariprakash@outlook.com>
2023-11-26 17:53:29 +0530

    Added option to specify subscription base tier.

--- src/utils/subscription.ts
+++ src/utils/subscription.ts
@@ -1,23 +1,32 @@
 import moment from "moment";
 import UserModel, { UserInterface } from "../model/mongo/user";
+import { Configuration } from "../singleton/configuration";
 
 export const checkSubscription = (input: UserInterface | UserInterface[]) => {
+  const baseTier = Configuration.get("user.subscription.base-tier");
   if (Array.isArray(input)) {
     const toUpdate = [];
     const users = input;
     for (let i = 0; i < users.length; i++) {
       if (users[i].isSubscribed && moment().isAfter(moment(users[i].subscriptionExpiry))) {
         users[i].isSubscribed = false;
+        users[i].subscriptionTier = baseTier;
         toUpdate.push(users[i]._id);
       }
     }
-    toUpdate.length && UserModel.updateMany({ _id: { $in: toUpdate } }, { $set: { isSubscribed: false } }, () => {});
+    toUpdate.length &&
+      UserModel.updateMany(
+        { _id: { $in: toUpdate } },
+        { $set: { isSubscribed: false, subscriptionTier: baseTier } },
+        () => {}
+      );
     return users;
   } else {
     const user = input;
     if (user.isSubscribed && moment().isAfter(moment(user.subscriptionExpiry))) {
       user.isSubscribed = false;
-      UserModel.updateOne({ _id: user._id }, { $set: { isSubscribed: false } }, () => {});
+      user.subscriptionTier = baseTier;
+      UserModel.updateOne({ _id: user._id }, { $set: { isSubscribed: false, subscriptionTier: baseTier } }, () => {});
     }
     return user;
   }
