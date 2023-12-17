418802cca6c510c400e7eac2e0f62770fce88717
Shrihari Prakash <shrihariprakash@outlook.com>
2023-12-17 16:18:00 +0530

    Added headsup tag in demo mode.

--- src/public/components/layout.js
+++ src/public/components/layout.js
@@ -125,6 +125,11 @@ export default function Layout({ children }) {
             </div>
             {configuration["content.sidebar.enabled"] && (
               <div className="sidebar" style={{ backgroundImage: `url(${sidebarImage})` }}>
+                {configuration["system.demo-mode"] &&
+                  <div class="ribbon">
+                    Demo Mode
+                  </div>
+                }
                 {configuration["content.sidebar.content-enabled"] && (
                   <div className="intro">
                     <div className="title1">{configuration["content.sidebar.intro.title1"]}</div>
