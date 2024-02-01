852e708d849d8c3c4df8f0020529b903c8c3ba41
Shrihari Prakash <shrihariprakash@outlook.com>
2024-02-01 22:22:58 +0530

    Added option to write logs to a file.

--- src/service/logger/logger.ts
+++ src/service/logger/logger.ts
@@ -1,17 +1,27 @@
 import pino from "pino";
 import pretty from "pino-pretty";
+import fs from "fs";
+
 import { Configuration } from "../../singleton/configuration";
 
 export class Logger {
   logger;
 
   constructor() {
-    const stream = pretty({
+    const prettyStream = pretty({
       colorize: true,
       translateTime: "yyyy-dd-mm HH:MM:ss",
     });
-    this.logger = pino(stream);
-    this.logger.level = Configuration.get("system.log-level") as string;
+
+    var streams: any[] = [{ level: Configuration.get("system.log-level"), stream: prettyStream }];
+
+    const logFilePath = Configuration.get("system.log-file-path");
+    if (logFilePath) {
+      const fileStream = fs.createWriteStream(logFilePath);
+      streams.push({ level: Configuration.get("system.log-level"), stream: fileStream });
+    }
+
+    this.logger = pino({ level: Configuration.get("system.log-level") }, pino.multistream(streams));
   }
 
   public getLogger(): any {
