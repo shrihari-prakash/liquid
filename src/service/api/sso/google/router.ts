25abfa4a9e0fe7075c1b436780812afb660639c3
Shri <35889246+shrihari-prakash@users.noreply.github.com>
2024-07-16 12:38:43 +0530

    Update router.ts

--- src/service/api/sso/google/router.ts
+++ src/service/api/sso/google/router.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { Passport } from "../../../../singleton/passport.js";
 import GET_GoogleCallback from "./callback.get.js";
-import GET_GoogleSuccess from "./success.get.js";
+import { POST_GoogleSuccessValidator }, POST_GoogleSuccess from "./success.get.js";
 
 const GoogleRouter = express.Router();
 
@@ -14,7 +14,7 @@ GoogleRouter.get(
 
 GoogleRouter.get("/callback", Passport.authenticate("google"), GET_GoogleCallback);
 
-GoogleRouter.get("/success", GET_GoogleSuccess);
+GoogleRouter.post("/success", GoogleSuccessValidator, POST_GoogleSuccess);
 
 export default GoogleRouter;
 
