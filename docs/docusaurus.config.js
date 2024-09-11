92ded9a56f060c49d161ac33d3f8f9d3dd49fee2
Shrihari Prakasam <Shrihari.Prakasam@genesys.com>
2024-09-11 21:09:29 +0530

    Improved fields documentation.

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -55,6 +55,11 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      colorMode: {
+        defaultMode: 'dark',
+        disableSwitch: false,
+        respectPrefersColorScheme: false,
+      },
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
