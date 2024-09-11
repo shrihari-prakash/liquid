66dbd6bd9858d195dcae6fa95c6e3c938dd980c8
Shrihari Prakasam <Shrihari.Prakasam@genesys.com>
2024-09-11 21:39:50 +0530

    Changed documentation config.

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/liquid-icon-mini.png',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://liquid.shrihariprakash.in',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/liquid/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
