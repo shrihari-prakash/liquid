40dbd8866cfd7f512a3e7a64bb0db1a454f24349
Shrihari Prakasam <Shrihari.Prakasam@genesys.com>
2024-10-09 14:22:24 +0530

    Added prefix to pusher event.

--- src/service/pusher/pusher.ts
+++ src/service/pusher/pusher.ts
@@ -32,14 +32,14 @@ export default class Pusher {
       log.info(
         "Push running on %s adapter for events %s.",
         Configuration.get("system.queue-adapter"),
-        Configuration.get("system.push-events")
+        Configuration.get("system.push-events"),
       );
     }
     switch (Configuration.get("system.queue-adapter")) {
       case adapters.RabbitMQ:
         if (!Configuration.get("privilege.can-use-rabbitmq")) {
           log.warn(
-            "Usage of push events is enabled. However, this requires option `Can Use RabbitMQ(privilege.can-use-rabbitmq)` to be true. Events will not be published until you setup RabbitMQ options."
+            "Usage of push events is enabled. However, this requires option `Can Use RabbitMQ(privilege.can-use-rabbitmq)` to be true. Events will not be published until you setup RabbitMQ options.",
           );
         }
         this.queue = RabbitMQ;
@@ -47,7 +47,7 @@ export default class Pusher {
       case adapters.Redis:
         if (!Configuration.get("privilege.can-use-cache")) {
           log.warn(
-            "Usage of push events is enabled. However, this requires option `Can Use Redis(privilege.can-use-cache)` to be true. Events will not be published until you setup Redis options."
+            "Usage of push events is enabled. However, this requires option `Can Use Redis(privilege.can-use-cache)` to be true. Events will not be published until you setup Redis options.",
           );
         }
         this.queue = RedisPublisher;
@@ -56,6 +56,8 @@ export default class Pusher {
   }
 
   async publish(event: PushEvent) {
+    const prefix = Configuration.get("system.push-events.prefix");
+    if (prefix) event.name = `${prefix}.${event.name}`;
     log.debug("Pusher publish called: %o", event);
     if (!this.queue) return;
     if (!Configuration.get("system.push-events").includes(event.name)) {
@@ -73,3 +75,4 @@ export default class Pusher {
     }
   }
 }
+
