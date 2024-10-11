5b41a822d11e7abc116b5f841509f91eb0faa5ea
Shrihari Prakasam <Shrihari.Prakasam@genesys.com>
2024-10-11 19:43:34 +0530

    Change default font to Inter.

--- src/public/utils/utils.js
+++ src/public/utils/utils.js
@@ -25,9 +25,9 @@ export function useFont(fontFace, fontUrl, onDone) {
   let appFontFace = fontFace;
   let appFontURL = fontUrl;
   if (!appFontFace && !appFontURL) {
-    appFontFace = "Poppins";
+    appFontFace = "Inter";
     appFontURL =
-      "https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,800&display=swap";
+      "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap";
   }
   console.log(`Switching to "${appFontFace}" as default font.`);
   console.log(`Loading font from ${appFontURL}.`);
